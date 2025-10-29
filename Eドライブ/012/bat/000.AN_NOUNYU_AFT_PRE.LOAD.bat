f3d9c63cb0a2f078a255911981d9f37dcf6500cd
Ochiai-FIT <y_ochiai@retail-community.jp>
2025-10-29 12:36:43 +0900

    012_bat_残り

---
+++ Eドライブ/012/bat/000.AN_NOUNYU_AFT_PRE.LOAD.bat
@@ -0,0 +1,36 @@
+REM [000][AN_NOUNYU_AFT_TEN.LOAD]
+@ECHO OFF
+
+REM ログフォルダ作成
+IF NOT EXIST %~DP0..\logs MKDIR %~DP0..\logs
+
+REM コマンドフォルダ作成
+IF NOT EXIST %~DP0..\cmd MKDIR %~DP0..\cmd
+
+REM ログファイルの情報
+SET OUTFILE=%~DP0../logs/setup.log
+SET OUTTIME=%~DP0../logs/time.log
+
+REM ベースフォルダ
+SET BASEDIR=%~DP0..
+
+REM 更新対象の情報
+SET INFOSCRIPT=%~DP0..\cmd\%~N0.DB2
+
+SET MSG=%DATE% %TIME% INAPRE.AN_NOUNYU_AFT ロード処理（処理前のINAIF.AN_NOUNYU_AFT を退避）
+ECHO %MSG%
+>> %OUTFILE% ECHO %MSG%
+
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv -e "TRUNCATE TABLE INAPRE.AN_NOUNYU_AFT  ;commit;" >>%OUTFILE% 2>&1
+IF NOT %ERRORLEVEL%==0 GOTO ERROR
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv -e"INSERT IGNORE INTO INAPRE.AN_NOUNYU_AFT SELECT * FROM INAIF.AN_NOUNYU_AFT;commit;" >>%OUTFILE% 2>&1
+IF NOT %ERRORLEVEL%==0 GOTO ERROR
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv -e "ANALYZE TABLE INAPRE.AN_NOUNYU_AFT;commit;" >>%OUTFILE% 2>&1
+IF NOT %ERRORLEVEL%==0 GOTO ERROR
+
+
+:FINAL
+EXIT /B 0
+
+:ERROR
+EXIT /B 1
