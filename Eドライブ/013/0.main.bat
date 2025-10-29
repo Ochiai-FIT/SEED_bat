9ff8b09cdc87b0a41654dd4ff725456899063038
Ochiai-FIT <y_ochiai@retail-community.jp>
2025-10-29 12:38:39 +0900

    job013_残り

---
+++ Eドライブ/013/0.main.bat
@@ -0,0 +1,666 @@
+@echo off
+cls
+
+rem 初期値()
+set event_output=0
+
+rem 接続情報取得
+set curpath=%~dp0..\
+call %curpath%._init.bat
+
+rem フォルダ名取得
+set folderpath=%~dp0
+set folderpath=%folderpath:~0,-1%
+for /F "delims=" %%a in ('echo "%folderpath%"') do set foldername=%%~nxa
+
+rem I/F リスト
+set readlist=%~dp0list_load.txt
+rem I/F 格納先フルパス
+set input=%~dp0data
+rem I/F 確認用バッチファイル
+set checkfile=%~dp0checkfile.bat
+
+rem I/F バックアップ用日時(YYYYMMDDHHMM)取得
+set ltime=%date:~-10,4%%date:~-5,2%%date:~-2%%time:~0,2%%time:~3,2%
+set ltime=%ltime: =0%
+
+rem 日付取得
+set days=%date:~8,2%
+
+rem ログファイル
+set outfile=%~dp0logs\setup.log
+set outtime=%~dp0logs\time.log
+set findfile=%~dp0logs\setup.find.log
+
+rem ログフォルダ作成
+if not exist %~dp0logs mkdir %~dp0logs
+
+rem データフォルダ作成
+if not exist %input% mkdir %input%
+
+rem エクスポートフォルダ作成
+if not exist %~dp0exp mkdir %~dp0exp
+
+
+rem ログフォルダ・ローテーション
+for %%F in (%outfile%) do if %%~zF GEQ 10485760 move /Y %outfile% %outfile%.old
+for %%F in (%outtime%) do if %%~zF GEQ 10485760 move /Y %outtime% %outtime%.old
+
+echo %DATE% %TIME% 日次処理(xx.Load_data)の開始 >> %outfile%
+
+
+rem ================ <プロセス切断> ==============
+rem SET callbat=bat\000.DISCONNECT.bat
+rem >> %outfile% ECHO %callbat%
+rem CALL %~dp0%callbat%
+rem IF "%ERRORLEVEL%" EQU "1" GOTO ERROR
+
+rem =====================================
+rem 000 各予約マスタから反映済を削除
+rem =====================================
+set callbat=bat\000.YOYAKU.DEL.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 008 商品基本マスタ(予約)
+rem =====================================
+set callbat=bat\008.AEA81.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem 商品マスタ_予約(前回処理結果)
+set callbat=bat\008.MSTSHN_Y_LT.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 009 仕入グループ商品(予約)
+rem =====================================
+set callbat=bat\009.AEA82.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem 仕入グループ_予約(前回処理結果)
+set callbat=bat\009.MSTSIRGPSHN_Y_LT.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 010 売価コントロール(予約)
+rem =====================================
+set callbat=bat\010.AEA83.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem 売価コントロール_予約(前回処理結果)
+set callbat=bat\010.MSTBAIKACTL_Y_LT.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 011 ソースコード管理(予約)
+rem =====================================
+set callbat=bat\011.AEA84.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem ソースコード管理_予約(前回処理結果)
+set callbat=bat\011.MSTSRCCD_Y_LT.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 001 商品基本マスタ
+rem =====================================
+set callbat=bat\001.AEA81.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem 商品マスタ(前回処理結果)
+set callbat=bat\001.MSTSHN_LT.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 002 仕入グループ商品
+rem =====================================
+set callbat=bat\002.AEA82.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem 仕入グループ商品(前回処理結果)
+set callbat=bat\002.MSTSIRGPSHN_LT.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 003 売価コントロール
+rem =====================================
+set callbat=bat\003.AEA83.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem 売価コントロール(前回処理結果)
+set callbat=bat\003.MSTBAIKACTL_LT.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 004 ソースコード管理
+rem =====================================
+set callbat=bat\004.AEA84.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem ソースコード管理(前回処理結果))
+set callbat=bat\004.MSTSRCCD_LT.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 026 商品店グループ
+rem =====================================
+set callbat=bat\026.AIA57.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 027 商品店グループ店舗
+rem =====================================
+set callbat=bat\027.AIA58.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 027 仕入グループ商品
+rem =====================================
+set callbat=bat\027.AEA82.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 027 仕入グループ商品(予約)
+rem =====================================
+set callbat=bat\027.AEA82_Y.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 027 売価コントロール
+rem =====================================
+set callbat=bat\027.AEA83.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 027 売価コントロール(予約)
+rem =====================================
+set callbat=bat\027.AEA83_Y.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 037 配送パターン
+rem =====================================
+set callbat=bat\037.AIC55.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 037 商品基本マスタ
+rem =====================================
+set callbat=bat\037.AEA81.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 037 商品基本マスタ(予約)
+rem =====================================
+set callbat=bat\037.AEA81_Y.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 037 仕入グループ商品
+rem =====================================
+set callbat=bat\037.AEA82.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 037 仕入グループ商品(予約)
+rem =====================================
+set callbat=bat\037.AEA82_Y.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 038 エリア別配送パターン
+rem =====================================
+set callbat=bat\038.AIC56.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 631 完了通知
+rem =====================================
+set callbat=bat\631.完了通知.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 028 店舗基本マスタ
+rem =====================================
+set callbat=bat\028.AFA01.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 030 店舗部門マスタ
+rem =====================================
+set callbat=bat\030.AFA02.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 032 店舗曜日別発注部門
+rem =====================================
+set callbat=bat\032.AIB07.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 033 店舗休日
+rem =====================================
+set callbat=bat\033.AIB06.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 634 完了通知
+rem =====================================
+set callbat=bat\634.完了通知.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 034 仕入先マスタ
+rem =====================================
+set callbat=bat\034.AFB01.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 035 配送パターン仕入先
+rem =====================================
+set callbat=bat\035.AIC52.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 036 エリア別配送パターン仕入先
+rem =====================================
+set callbat=bat\036.AIC53.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 039 複数仕入先＿実仕入先
+rem =====================================
+set callbat=bat\039.AIC57.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 040 複数仕入先＿店舗
+rem =====================================
+set callbat=bat\040.AIC58.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 637 完了通知
+rem =====================================
+set callbat=bat\637.完了通知.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 087 発注用複数仕入先
+rem =====================================
+set callbat=bat\087.A11MSIR.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 041 配送グループ
+rem =====================================
+set callbat=bat\041.A11HGM.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 042 配送店グループ
+rem =====================================
+set callbat=bat\042.A11HGT.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 043 配送店グループ店舗
+rem =====================================
+set callbat=bat\043.A11HGT.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 043 エリア別配送パターン仕入先
+rem =====================================
+set callbat=bat\043.AIC53.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 043 エリア別配送パターン
+rem =====================================
+set callbat=bat\043.AIC56.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 640 完了通知
+rem =====================================
+set callbat=bat\640.完了通知.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 044 部門マスタ
+rem =====================================
+set callbat=bat\044.AFC01.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 045 大分類マスタ
+rem =====================================
+set callbat=bat\045.ARD02.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 046 中分類マスタ
+rem =====================================
+set callbat=bat\046.ARD03.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 047 小分類マスタ
+rem =====================================
+set callbat=bat\047.ARD04.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 048 小小分類マスタ
+rem =====================================
+set callbat=bat\048.ARD05.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 643 完了通知
+rem =====================================
+set callbat=bat\643.完了通知.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 058 リードタイム
+rem =====================================
+set callbat=bat\058.AIF51.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 059 メーカー
+rem =====================================
+set callbat=bat\059.AFD02.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 060 税率
+rem =====================================
+set callbat=bat\060.AIF52.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 061 名称
+rem =====================================
+set callbat=bat\061.A11NAME.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 062 名称変換エラーデータ
+rem =====================================
+set callbat=bat\062.A11ERR.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 646 完了通知
+rem =====================================
+set callbat=bat\646.完了通知.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 081 発注用商品マスタ
+rem =====================================
+rem 発注用商品マスタ
+set callbat=bat\081.HA_A11SHOH.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+rem export
+set callbat=bat\081.A11SHOH.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 088 発注用商品マスタ（予約）
+rem =====================================
+rem 発注用商品マスタ（予約）
+set callbat=bat\088.HA_A11SHOH_Y.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem export
+set callbat=bat\088.A11SHOH.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 083 発注用店舗部門マスタ
+rem =====================================
+rem 発注用店舗部門マスタ
+set callbat=bat\083.HA_A11TBMH.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem export
+set callbat=bat\083.A11TBMH.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 082 発注用仕入売価店舗展開
+rem =====================================
+rem 発注用仕入売価店舗展開
+set callbat=bat\082.HA_A11TSH.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem export
+set callbat=bat\082.A11TSH.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 089 発注用仕入売価店舗展開（予約）
+rem =====================================
+rem 発注用仕入売価店舗展開（予約）
+set callbat=bat\089.HA_A11TSH_Y.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem export
+set callbat=bat\089.A11TSH.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 084 発注用配送パターン仕入先
+rem =====================================
+rem 発注用配送パターン仕入先
+set callbat=bat\084.HA_A11HSR.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem export
+set callbat=bat\084.A11HSR.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 085 発注用配送パターン仕入先店舗（エリア）
+rem =====================================
+rem 発注用配送パターン仕入先店舗（エリア）
+set callbat=bat\085.HA_A11EHSR_A.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem export
+set callbat=bat\085.A11EHSR.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 086 発注用配送パターン仕入先店舗（店Ｇ）
+rem =====================================
+rem 発注用配送パターン仕入先店舗（店Ｇ）
+set callbat=bat\086.HA_A11EHSR_G.LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem export
+set callbat=bat\086.A11EHSR.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+rem =====================================
+rem 649 完了通知
+rem =====================================
+set callbat=bat\649.完了通知.EXP.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+
+REM ===== BACKUP COPY ====================
+setlocal
+set x=%~dp0
+set x=%x:~0,-1%
+for /F "delims=" %%a in ('echo "%x%"') do set DIRNAME=%%~na
+ROBOCOPY %~dp0EXP E:\MDM\%DIRNAME%\%ltime% /R:0 /E /NFL /NP /XD backup bkup RcvTemp >> %outfile%
+endlocal
+
+:SUCCESS
+echo %DATE% %TIME% 日次処理(Daily)が正常に終了しました >> %outfile%
+
+:LAST
+EXIT /B 0
+
+:ERROR
+ECHO %DATE% %TIME% %callbat% エラーが発生しました(RC=1) >> %outfile%
+EXIT /B 1
