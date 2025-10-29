b356464e2bd9d63d5bfea9642fd109e559cb1ade
Ochiai-FIT <y_ochiai@retail-community.jp>
2025-10-29 13:51:21 +0900

    023_bat_残り2

---
+++ Eドライブ/023/bat/669.完了通知.EXP.bat
@@ -0,0 +1,43 @@
+REM [669][完了通知]
+@ECHO OFF
+
+REM ログフォルダ作成
+IF NOT EXIST %~DP0..\logs MKDIR %~DP0..\logs
+
+REM コマンドフォルダ作成
+IF NOT EXIST %~DP0..\cmd MKDIR %~DP0..\cmd
+
+REM ログファイルの情報
+SET OUTFILE=%~DP0../logs/setup.log
+SET OUTTIME=%~DP0../logs/time.log
+
+REM ベースフォルダ
+SET BASEDIR=%~DP0..
+
+REM 更新対象の情報
+SET INFOSCRIPT=%~DP0..\cmd\%~N0.DB2
+SET EXPFILENAME=%~DP0..\exp\0669
+SET OUTFILENAME=0669
+SET HULFTID=VFBUO022
+
+REM ===== HULFT SEND DELETE ===============
+SET HULFT=D:\HULFTFILES\SEND\%OUTFILENAME%
+DEL /Q %HULFT%\*
+
+SET MSG=%DATE% %TIME%  →  EXPORT処理(完了通知 19締め全店特売（アンケート有）)※空ファイル
+ECHO %MSG%
+ECHO %MSG% >> %OUTFILE%
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% -N -e "SELECT * FROM (SELECT 1 AS DUMMY) DUMMY WHERE 1 = 2" >%EXPFILENAME% 2>>%OUTFILE%
+IF NOT %ERRORLEVEL%==0 GOTO ERROR
+
+REM ===== HULFT SEND MOVE =================
+ROBOCOPY %~dp0..\EXP %HULFT% %OUTFILENAME% /R:0 /NFL /NP >> %OUTFILE%
+
+REM ===== HULFT SEND =======================
+call %~dp0..\..\HULFT\.start_HULFT_SEND.bat %HULFTID% %OUTFILENAME%
+
+:FINAL
+EXIT /B 0
+
+:ERROR
+EXIT /B 1
