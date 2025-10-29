70460a8df54d0b9a9de7d11095dcaadef83b171d
Ochiai-FIT <y_ochiai@retail-community.jp>
2025-10-29 13:56:23 +0900

    job026_残り

---
+++ Eドライブ/026/0.main.bat
@@ -0,0 +1,88 @@
+@echo off
+cls
+
+rem 初期値()
+set event_output=0
+
+rem 接続情報取得
+set curpath=%~dp0..\
+call %curpath%._init.bat
+
+rem フォルダ名取得
+set folderpath=%~dp0
+set folderpath=%folderpath:~0,-1%
+for /F "delims=" %%a in ('echo "%folderpath%"') do set foldername=%%~nxa
+
+rem I/F リスト
+set readlist=%~dp0list_load.txt
+rem I/F 格納先フルパス
+set input=%~dp0data
+rem I/F 確認用バッチファイル
+set checkfile=%~dp0checkfile.bat
+
+rem I/F バックアップ用日時(YYYYMMDDHHMM)取得
+set ltime=%date:~-10,4%%date:~-5,2%%date:~-2%%time:~0,2%%time:~3,2%
+set ltime=%ltime: =0%
+
+rem 日付取得
+set days=%date:~8,2%
+
+rem ログファイル
+set outfile=%~dp0logs\setup.log
+set outtime=%~dp0logs\time.log
+set findfile=%~dp0logs\setup.find.log
+
+rem ログフォルダ作成
+if not exist %~dp0logs mkdir %~dp0logs
+
+rem データフォルダ作成
+if not exist %input% mkdir %input%
+
+rem エクスポートフォルダ作成
+if not exist %~dp0exp mkdir %~dp0exp
+
+
+rem ログフォルダ・ローテーション
+for %%F in (%outfile%) do if %%~zF GEQ 10485760 move /Y %outfile% %outfile%.old
+for %%F in (%outtime%) do if %%~zF GEQ 10485760 move /Y %outtime% %outtime%.old
+
+echo %DATE% %TIME% 日次処理(xx.Load_data)の開始 >> %outfile%
+
+
+rem ================ <プロセス切断> ==============
+rem SET callbat=bat\000.DISCONNECT.bat
+rem >> %outfile% ECHO %callbat%
+rem CALL %~dp0%callbat%
+rem IF "%ERRORLEVEL%" EQU "1" GOTO ERROR
+
+REM ===== HULFT REV MOVE =================
+DEL /Q %~dp0\DATA\*
+ROBOCOPY  D:\HULFTFILES\RCV\0069 %~dp0DATA /R:0 /E /NFL /NP /XD backup bkup RcvTemp /MOV >> %OUTFILE%
+
+REM ===== BACKUP COPY ====================
+setlocal
+set x=%~dp0
+set x=%x:~0,-1%
+for /F "delims=" %%a in ('echo "%x%"') do set DIRNAME=%%~na
+ROBOCOPY %~dp0DATA E:\MDM\%DIRNAME%\%ltime% /R:0 /E /NFL /NP /XD backup bkup RcvTemp >> %outfile%
+endlocal
+
+rem ===== No.069 変換 ======================
+call %~dp0..\CNV\J026Converter.bat %~dp0data\ J069.txt 0069
+
+rem ===== LOAD ======================
+set callbat=bat\026.069_LOAD.bat
+echo %callbat% >> %outfile%
+call %~dp0%callbat%
+if errorlevel 1 goto error
+
+
+:SUCCESS
+echo %DATE% %TIME% 処理が正常に終了しました >> %outfile%
+
+:LAST
+EXIT /B 0
+
+:ERROR
+ECHO %DATE% %TIME% %callbat% エラーが発生しました(RC=1) >> %outfile%
+EXIT /B 1
