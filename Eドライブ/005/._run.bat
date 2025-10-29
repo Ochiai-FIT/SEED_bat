4386d75791626f34687a0353ca97a5ff9b988096
ochiai <y_ochiai@retail-community.jp>
2025-10-29 11:09:52 +0900

    job005_runバッチ

--- Eドライブ/005/._run.bat
+++ Eドライブ/005/._run.bat
@@ -1,1 +1,15 @@
+@echo off
+REM 状況確認
+REM Get-ExecutionPolicy
 
+REM ポリシー変更（管理者権限で開く）
+REM Set-ExecutionPolicy RemoteSigned
+
+REM カレントフォルダ固定
+pushd %~dp0
+
+REM プロセスチェック後にバッチ開始
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command "%~dp0\.process.ps1 .start.bat;exit $LASTEXITCODE"
+
+IF ERRORLEVEL 1 EXIT /B 1
+EXIT /B 0
