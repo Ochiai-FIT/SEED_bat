4c8e5d4f6823006b8ce0a949b1449e2dbe8c5134
ochiai <y_ochiai@retail-community.jp>
2025-10-29 13:40:37 +0900

    022_bat

---
+++ Eドライブ/022/bat/022.UPDATE.bat
@@ -0,0 +1,45 @@
+REM [015][UPDATE]
+@ECHO OFF
+
+REM ログフォルダ作成
+IF NOT EXIST %~DP0..\logs MKDIR %~DP0..\logs
+
+REM コマンドフォルダ作成
+IF NOT EXIST %~DP0..\cmd MKDIR %~DP0..\cmd
+
+REM ログファイルの情報
+SET OUTFILE=%~DP0../logs/setup.log
+SET OUTTIME=%~DP0../logs/time.log
+
+REM ベースフォルダ
+SET BASEDIR=%~DP0..
+
+REM 更新対象の情報
+SET INFOSCRIPT=%~DP0..\cmd\%~N0.DB2
+
+
+SET INFILENAME=
+SET MSG=%DATE% %TIME% 商品 INAMS.MSTSHN UPDATE処理（送信フラグ=1 & プライスカード発行フラグ=1）
+ECHO %MSG%
+>> %OUTFILE% ECHO %MSG%
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv -e "UPDATE INAMS.MSTSHN SET PCARD_OPFLG = 0 WHERE PCARD_OPFLG=1 AND SENDFLG=1; COMMIT;" >>%OUTFILE% 2>&1
+
+SET MSG=%DATE% %TIME% 商品店グループ INAMS.MSTSHNTENGP UPDATE処理（送信フラグ=1 & プライスカード発行フラグ=1）
+ECHO %MSG%
+>> %OUTFILE% ECHO %MSG%
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv -e "UPDATE INAMS.MSTSHNTENGP SET PCARD_OPFLG = 0 WHERE PCARD_OPFLG=1 AND SENDFLG=1; COMMIT;" >>%OUTFILE% 2>&1
+
+SET MSG=%DATE% %TIME% 商品＿予約 INAMS.MSTSHN_Y UPDATE処理（送信フラグ=1 & プライスカード発行フラグ=1）
+ECHO %MSG%
+>> %OUTFILE% ECHO %MSG%
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv -e "UPDATE INAMS.MSTSHN_Y SET PCARD_OPFLG = 0 WHERE PCARD_OPFLG=1 AND SENDFLG=1; COMMIT;" >>%OUTFILE% 2>&1
+
+
+rem IF NOT %ERRORLEVEL%==0 GOTO ERROR
+
+
+:FINAL
+EXIT /B 0
+
+:ERROR
+EXIT /B 1
