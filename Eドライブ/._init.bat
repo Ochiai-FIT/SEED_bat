ccc4d05f8693081c20c3fb956df7244c83210173
Ochiai-FIT <y_ochiai@retail-community.jp>
2025-10-29 10:59:50 +0900

    DB情報

---
+++ Eドライブ/._init.bat
@@ -0,0 +1,14 @@
+rem MySQLSQLのbinディレクトリ
+set MYSQLPATH=C:\Program Files\MySQL\MySQL Server 8.0\bin\
+
+rem 接続先情報
+set HOST=seed-main.cluster-c40hu4ijseiq.ap-northeast-1.rds.amazonaws.com
+set DB_NAME=mysql
+set MYSQL_PWD=Passw0rd
+set USER_ID=admin
+set OPTION=--default-character-set=cp932
+
+rem イベントログ
+set event_output=0
+set db=開発
+
