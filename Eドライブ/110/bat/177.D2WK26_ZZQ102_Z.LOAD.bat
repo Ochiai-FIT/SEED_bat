7db1879ac9ecc48b3c0335a3f139e4193c7f37e1
Ochiai-FIT <y_ochiai@retail-community.jp>
2025-10-30 11:12:56 +0900

    110_bat_残り

---
+++ Eドライブ/110/bat/177.D2WK26_ZZQ102_Z.LOAD.bat
@@ -0,0 +1,38 @@
+REM [177][D2WK26_ZZQ102_Z]
+@ECHO OFF
+
+REM ログフォルダ作成
+IF NOT EXIST %~DP0..\logs MKDIR %~DP0..\logs
+
+REM コマンドフォルダ作成
+IF NOT EXIST %~DP0..\cmd MKDIR %~DP0..\cmd
+
+REM ログファイルの情報
+SET OUTFILE=%~DP0../logs/setup.log
+SET OUTTIME=%~DP0../logs/time.log
+
+REM ベースフォルダ
+SET BASEDIR=%~DP0..
+
+REM 更新対象の情報
+SET INFOSCRIPT=%~DP0..\cmd\%~N0.DB2
+
+REM SET MSG=%DATE% %TIME% INAWK.D2WK26_ZZQ102_Z(店別定量特売ファイルワーク7(全店特売)) テーブルクリア処理
+REM ECHO %MSG%
+REM >> %OUTFILE% ECHO %MSG%
+REM DB2 -L %OUTTIME% "TRUNCATE TABLE INAWK.D2WK26_ZZQ102_Z IMMEDIATE" >> %OUTFILE%
+REM IF NOT %ERRORLEVEL%==0 GOTO ERROR
+
+SET MSG=%DATE% %TIME% INAWK.D2WK26_ZZQ102_Z(店別定量特売ファイルワーク7(全店特売)) ロード処理（ワークテーブル作成）
+ECHO %MSG%
+>> %OUTFILE% ECHO %MSG%
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv   <  %~dp0177.D2WK26_ZZQ102_Z.LOAD.sql >>%OUTFILE% 2>&1
+IF NOT %ERRORLEVEL%==0 GOTO ERROR
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv -e  "ANALYZE TABLE INAWK.D2WK26_ZZQ102_Z;commit;" >>%OUTFILE% 2>&1
+IF NOT %ERRORLEVEL%==0 GOTO ERROR
+
+:FINAL
+EXIT /B 0
+
+:ERROR
+EXIT /B 1
---
+++ Eドライブ/110/bat/182.D2WK28_ZZQ113_3.LOAD.bat
@@ -0,0 +1,39 @@
+REM [182][D2WK28_ZZQ113_3]
+@ECHO OFF
+
+REM ログフォルダ作成
+IF NOT EXIST %~DP0..\logs MKDIR %~DP0..\logs
+
+REM コマンドフォルダ作成
+IF NOT EXIST %~DP0..\cmd MKDIR %~DP0..\cmd
+
+REM ログファイルの情報
+SET OUTFILE=%~DP0../logs/setup.log
+SET OUTTIME=%~DP0../logs/time.log
+
+REM ベースフォルダ
+SET BASEDIR=%~DP0..
+
+REM 更新対象の情報
+SET INFOSCRIPT=%~DP0..\cmd\%~N0.DB2
+
+SET MSG=%DATE% %TIME% INAWK.D2WK28_ZZQ113_3(生活応援_商品3) テーブルクリア処理
+ECHO %MSG%
+>> %OUTFILE% ECHO %MSG%
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv -e "TRUNCATE TABLE INAWK.D2WK28_ZZQ113_3 ;commit;" >>%OUTFILE% 2>&1
+IF NOT %ERRORLEVEL%==0 GOTO ERROR
+
+SET MSG=%DATE% %TIME% INAWK.D2WK28_ZZQ113_3(生活応援_商品3) ロード処理（ワークテーブル作成）
+ECHO %MSG%
+>> %OUTFILE% ECHO %MSG%
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv < %~dp0182.D2WK28_ZZQ113_3.LOAD.sql  >>%OUTFILE% 2>&1
+IF NOT %ERRORLEVEL%==0 GOTO ERROR
+
+"%MYSQLPATH%mysql" %OPTION% -h %HOST% %DB_NAME% -u %USER_ID% --show-warnings -vv -e "ANALYZE TABLE INAWK.D2WK28_ZZQ113_3;" >>%OUTFILE% 2>&1
+IF NOT %ERRORLEVEL%==0 GOTO ERROR
+
+:FINAL
+EXIT /B 0
+
+:ERROR
+EXIT /B 1
